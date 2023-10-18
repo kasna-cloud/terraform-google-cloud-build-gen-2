949fbcebd085b921cd8892d3f94bb68af99cad75
Nathan <nathan.gauci@kasna.com.au>
2023-10-18 15:34:09 +1100

    Adding examples

---
+++ examples/GitHub/main.tf
@@ -0,0 +1,17 @@
+module "cloudbuild_v2" {
+  # source = "github.com/kasna-cloud/terraform-google-cloud-build-gen-2"
+  source = "../../"
+
+  project_id          = "your-project-id"
+  connection_name     = "example-github"
+  create_connection   = true # set to true once the secrets are added
+  git_provider        = "github"
+  connection_location = "us-central1"
+  installation_id     = 42798096
+
+
+  labels = {
+    test = "val"
+  }
+
+}
---
+++ examples/GitHub_enterprise/main.tf
@@ -0,0 +1,20 @@
+module "cloudbuild_v2" {
+  # source = "github.com/kasna-cloud/terraform-google-cloud-build-gen-2"
+  source = "../../"
+
+  project_id          = "your-project-id"
+  connection_name     = "example-ghe"
+  create_connection   = true # set to true once the secrets are added
+  git_provider        = "ghe"
+  connection_location = "us-central1"
+
+  git_url = "https://github.example.com"
+
+  app_id          = 3
+  installation_id = 1
+  app_slug        = "google-cloud-build"
+
+  labels = {
+    test = "val"
+  }
+}
